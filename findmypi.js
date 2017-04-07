1f18c32e1d83631846cfb321741cec59e52e8483
woutermeek <woutermeek@gmail.com>
2017-04-07 13:03:38 +0200

    update for latest builds

--- findmypi.js
+++ findmypi.js
@@ -1,9 +1,22 @@
-var dial = require('dial'),
-	url = require('url');
+var dgram = require('dgram');
+var st = 'urn:metrological-com:service:webbridge:1';
+var mSearchRequester = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+var message =
+    "M-SEARCH * HTTP/1.1\r\n"+
+    "ST:"+st+"\r\n"+
+    "Content-Length:1\r\n\r\n"+
+    "0\r\n\r\n";
 
-dial.on('device', function (device) {
-	if (new RegExp('wpe|webbridge', 'i').test(device.name))
-		console.log('found: ' + url.parse(device.location).hostname);
+mSearchRequester.on('message', function (mes,res) {
+    console.log('found pi: ' + res.address);
+});
+mSearchRequester.on('listening', function () {
+    mSearchRequester.send(new Buffer(message, "ascii"), 0, message.length, 1900, '239.255.255.250', function (err,res) {
+    });
 });
 
-dial.discover();
+mSearchRequester.bind(1900, '192.168.0.14');
+setTimeout(function(){
+    mSearchRequester.close();
+    process.exit();
+}, 2000);
